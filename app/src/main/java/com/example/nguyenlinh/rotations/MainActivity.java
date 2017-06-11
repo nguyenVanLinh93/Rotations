80ae8946076d5d712d48f531175e52259c32d594
nguyenlinh <linh.nguyen93dhv@gmail.com>
2017-06-11 21:01:04 +0700

    change manifest and main save and restore data

--- app/src/main/java/com/example/nguyenlinh/rotations/MainActivity.java
+++ app/src/main/java/com/example/nguyenlinh/rotations/MainActivity.java
@@ -8,25 +8,34 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 /**
  * Rotations and App Restarts
  * Make an app that lets the user enter a short string.
  * Have an “Add Message” button, that,when pressed, adds the message to the bottom of the screen.
- *
+ * <p>
  * When you rotate the app, make sure that the TextView text is preserved.
  * When you rotate the app, use a different layout for the prompt, EditText, and Button.
+ *
+ * @author nguyenlinh
+ * @version 1.0
+ * @since 2017-6-10
  */
 public class MainActivity extends AppCompatActivity {
 
     private EditText mEditText;
     private Button mButton;
+    private ArrayList mListText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mEditText = (EditText) findViewById(R.id.editText);
-        mButton = (Button)findViewById(R.id.button);
+        mButton = (Button) findViewById(R.id.button);
+        mListText = new ArrayList();
 
         mButton.setOnClickListener(new onclick());
     }
@@ -35,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onClick(View v) {
             String someText = String.valueOf(mEditText.getText());
-            LinearLayout mainlayouts = (LinearLayout)findViewById(R.id.activity_main);
+            LinearLayout mainlayouts = (LinearLayout) findViewById(R.id.activity_main);
 
             /*
             Note 1: to programmatically add a TextView to the bottom of a vertical LinearLayout,
@@ -43,6 +52,38 @@ public class MainActivity extends AppCompatActivity {
              */
             TextView textV = new TextView(MainActivity.this);
             textV.setText(someText);
+            mListText.add(someText);
+            mainlayouts.addView(textV);
+        }
+    }
+
+    /**
+     * save data
+     *
+     * @param outState
+     */
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putStringArrayList("textdata", mListText); // chỉ lưu dc các kiểu nguyên thuỷ
+    }
+
+    /**
+     * restore data
+     *
+     * @param savedInstanceState
+     */
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        ArrayList restoreData = savedInstanceState.getStringArrayList("textdata");
+
+        for (int i = 0; i < restoreData.size(); i++) {
+            String someText = (String) restoreData.get(i);
+            LinearLayout mainlayouts = (LinearLayout) findViewById(R.id.activity_main);
+            TextView textV = new TextView(MainActivity.this);
+            textV.setText(someText);
+            mListText.add(someText); // phải có nếu ko chỉ save 1 lần còn lần thứ 2 sẽ ko lưu dc
             mainlayouts.addView(textV);
         }
     }
